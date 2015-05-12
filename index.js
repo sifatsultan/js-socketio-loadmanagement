419627ead8453e2647d70d4de1d87dab4d77f7f4
unknown <sifatsultan@gmail.com>
2015-05-12 21:43:04 +0400

    create table number and display it

--- index.js
+++ index.js
@@ -59,6 +59,82 @@ io.on('connection', function(socket) {
         
     })
 
+    socket.on('get 5th data', function() {
+        console.log('I am about to do a query')
+        connection.query('select * from notes LIMIT 4,1', function(err, rows, fields){
+            console.log('I just finished my query')
+            for(var i=0;i<rows.length; i++)
+                console.log('I just got the 5th data: '+rows[i].note);
+            socket.emit('show 5th data', rows)
+        })        
+    })
+    
+    /*
+CREATE TABLE pet (
+    name VARCHAR(20), 
+    owner VARCHAR(20),
+    species VARCHAR(20), 
+    sex CHAR(1), 
+    birth DATE, 
+    death DATE
+);    
+    */
+    
+    
+    /*
+connection.query('select * from notes LIMIT 4,1', function(err, rows, fields){    
+    
+    */
+    
+    var sqlCreateNumber = 'create table numbers(number int(8));'
+    var sqlGetNumbers= 'select * from numbers'
+    var sqlGet5thNumber = 'select * from numbers limit 4,1' 
+    var sqlGet1stNumber = 'select * from numbers limit 0,1'
+
+    var orderedNumbers = [];
+    for(var i=0; i<100; i++)
+        orderedNumbers[i] = i;
+    
+    socket.on('create new table', function(){
+    })
+    
+    /*connection.query('insert into notes (note) values (?)', msg)*/
+    socket.on('get numbers',function(){
+        connection.query(sqlCreateNumber)
+        console.log('btw I just created a new table called numbers')
+        for(var i=0; i<orderedNumbers.length; i++)
+            connection.query('insert into numbers (number) values(?)', orderedNumbers[i]);
+        
+        connection.query(sqlGetNumbers, function(err, rows, fields){
+            socket.emit('show numbers', rows)
+        })
+    })
+
+    
+    socket.on('do average', function(){
+        var firstNumber,
+            secondNumber;
+        
+        connection.query(sqlGet1stNumber, function(err, rows, fields){
+            firstNumber = rows.number
+            console.log('I just saved the first number')
+            
+            connection.query(sqlGet5thNumber, function(err, rows, fields){
+                secondNumber = rows.number
+                console.log('I just saved the fifth number')
+            
+                var average = (firstNumber + secondNumber)/2
+                console.log('done with averaging')
+        
+                socket.emit('display average', average)
+                console.log('threw an event to front end called "display average"')
+            })
+        })
+
+        
+        
+    })
+
 
 });
 
--- public/functions.js
+++ public/functions.js
@@ -16,6 +16,18 @@ $('#show').click(function() {
     socket.emit('get data');
 })
 
+$('#show5').click(function() {
+    socket.emit('get 5th data');
+})
+
+$('#numbers').click(function() {
+    socket.emit('get numbers');
+})
+
+$('#average').click(function(){
+    socket.emit('do average')
+})
+
 socket.on('chat message', function(msg) {
     $('#messages').append($('<li class="list-group-item">').text(msg));
 })
@@ -24,7 +36,28 @@ socket.on('show data', function(data) {
     $('#display').empty()
     console.log('so I got the following data from the back end\n')
     for(var i=0; i<data.length; i++) 
-        console.log(data[i].note)
+        console.log(data[i].note);
+    
+    //show the last two data
     for (var i = 0; i < data.length; i++)
         $('#display').append($('<li class="list-group-item">').text(data[i].note))
+
 })
+
+socket.on('show 5th data', function(data){
+    $('#display').empty()
+    for(var i=0; i<data.length; i++)
+        $('#display').append($('<li class="list-group-item">').text(data[i].note))
+})
+
+socket.on('show numbers', function(data){
+    $('#display').empty()
+    for(var i=0; i<data.length; i++)
+        $('#display').append($('<li class="list-group-item">').text(data[i].number))
+})
+
+socket.on('display average', function(data){
+    $('#display').empty()
+    $('#display').append($('<li class="list-group-item">').text(data))    
+})
+
